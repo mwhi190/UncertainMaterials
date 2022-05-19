e4f5112b57d4629f49e7a8816a4d6b491d5ba617
mwhi190 <68970854+mwhi190@users.noreply.github.com>
2022-05-19 16:45:24 +1200

    Add files via upload
    Models to solve ode, and optimize curve fit

---
+++ models.py
@@ -0,0 +1,8 @@
+# files containing functions to solve ode, and optimize for inverse model
+
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import integrate
+from scipy import interpolate
+from scipy import optimize
+import math
